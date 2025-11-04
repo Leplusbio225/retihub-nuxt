3674d8cacb4d30e5332e5b65a2e8598b911eaf5b
Leplusbio225 <emmanuelkacou225@outlook.com>
2025-11-04 10:01:52 +0000

    feat: mise à jour de la page "À propos" avec une description détaillée de RetiHub et ajout des icônes de réseaux sociaux

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,3 +1,67 @@
 <template>
-    <p>About page</p>
+    <div
+        class="flex justify-center flex-col bg-[#0404093b] space-y-4 mx-20 rounded p-4 text-slate-300 text-justify">
+        <p>
+            <span class="font-bold text-xl text-[#570076]">RetiHub🪄</span> est une plateforme qui simplifie la
+            recherche et la récupération d’objets perdus. Sa force repose sur ses utilisateurs ainsi que sa communauté.
+        </p>
+
+        <p>
+            L'idée est née après une observation des difficultés que rencontrait la plupart sur Facebook lors de la
+            perte d'un objet précieux tels que la Carte CNI, le passeport et bien d'autres.
+            Il fallait passer par des publications qui nécessitaient un partage massif dans l'espoir de rentrer en
+            contact avec la personne en possession de l'objet recherché.
+        </p>
+
+        <p>
+            <span class="font-bold text-xl text-[#570076]">RetiHub🪄</span> donc vise à faciliter ce processus en
+            servant d'intermédiaire entre les <b class="text-orange-500">**Finder**</b> et les <b
+                class="text-orange-500">**Owner**</b>. Nous pensons donc que chacun
+            de vous peut apporter sa pierre à l'édifice en participant d'une manière ou d'une autre afin de rendre cette
+            mission possible.
+        </p>
+
+        <div>
+            <p>
+                Le projet est <i class="underline">open source</i> et est entièrement ouvert à toute contribution.
+            </p>
+            <p>Nous croyons en la force de l'Open Source et en l'espoir de contribuer à rendre ce monde de plus en
+                plus meilleur par l'action de tout un chacun.
+            </p>
+        </div>
+
+        <p>
+            Nous comptons sur vous pour faire de cette plateforme la plus
+            grande communauté de partage et d'entraide qui puisse exister afin de donner le sourire à chacun de vous.
+        </p>
+
+        <div class="space-y-2">
+            <p>Lien du repo: <a class="hover:text-orange-500" href="https://github.com/Leplusbio225/retihub-nuxt.git" target="_blank">www.github.com/Leplusbio225/retihub-nuxt.git</a></p>
+            
+            <ul class="space-y-4">
+                
+                <li class="space-y-4">
+                    <span class="font-bold text-xl">Collaboration / Réseaux:</span>
+                    <ul class="space-y-2 mt-2 flex items-center gap-4">
+                        <li class="flex">
+                            <span><img src="~/assets/svg/linkedin.svg" alt="Linkedin icon"></span>
+                            <NuxtLink to="https://linkedin.com/in/emmanuel-kacou-74397b247">Linkedin</NuxtLink>
+                        </li>
+                        <li class="flex">
+                            <span><img src="~/assets/svg/github.svg" alt="Github icon"></span>
+                            <NuxtLink to="https://github.com/leplusbio225">Github</NuxtLink>
+                        </li>
+                        <li class="flex">
+                            <span><img src="~/assets/svg/facebook.svg" alt="Facebook icon"></span>
+                            <NuxtLink to="https://facebook.com/emmanuel.kacou.680355">Facebook</NuxtLink>
+                        </li>
+                        <li class="flex mb-1">
+                            <span><img src="~/assets/svg/yahoo.svg" alt="yahoo icon"></span>
+                            <NuxtLink to="mailto:kacouemmanuel225@yahoo.com">Email</NuxtLink>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
