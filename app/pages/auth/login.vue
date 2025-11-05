8dfc687c32cfe28c70829c614f49638c67a930c0
Leplusbio225 <emmanuelkacou225@outlook.com>
2025-11-05 00:02:30 +0000

    feat: ajout des pages de connexion et d'inscription avec mise en page d'authentification

---
+++ app/pages/auth/login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="h-full">
+
+    </div>
+</template>
+
+<script setup>
+
+definePageMeta({
+    layout: 'auth-layout'
+})
+</script>
---
+++ app/pages/auth/signup.vue
@@ -0,0 +1,29 @@
+<template>
+    <p class="text-xl text-center">Page d'inscription</p>
+
+    <!-- Signup button -->
+    <div class="grid place-content-center">
+        <NuxtLink class="border border-white text-white text-center p-2 rounded-2xl">
+            Continuer avec Google
+        </NuxtLink>
+    </div>
+
+    <p id="divider" class="text-center font-bold text-xl divide-cyan-950">
+        <span>OU</span>
+    </p>
+
+    <div class="p-2">
+        <form action="#" method="post">
+            <label for="first_name" class="grid gap-1">
+                Nom
+                <Input:button></Input:button>
+            </label>
+        </form>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'auth-layout'
+})
+</script>
