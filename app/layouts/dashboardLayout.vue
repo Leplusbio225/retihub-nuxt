9f8f566e00b2c82a990a24ff391cc21053289994
Leplusbio225 <emmanuelkacou225@outlook.com>
2025-11-07 09:00:35 +0000

    feat: ajout de la navigation et des sous-menus dans le layout du tableau de bord

--- app/layouts/dashboardLayout.vue
+++ app/layouts/dashboardLayout.vue
@@ -1,3 +1,98 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const open = ref(false)
+
+const links = [
+    [
+        {
+            label: 'Dashboard',
+            icon: 'i-lucide-house',
+            to: '/dashboard',
+            class: 'text-[#BF6BC7]',
+            onSelect: () => {
+                open.value = false
+            },
+            active: true
+        },
+        {
+            label: 'Inbox',
+            icon: 'i-lucide-inbox',
+            to: '/inbox',
+            badge: '4',
+            onSelect: () => {
+                open.value = false
+            }
+        }, {
+            label: 'Paramètres',
+            to: '/parametres',
+            icon: 'i-lucide-settings',
+            defaultOpen: true,
+            type: 'trigger',
+            children: [{
+                label: 'General',
+                to: '/settings',
+                exact: true,
+                onSelect: () => {
+                    open.value = false
+                }
+            }, {
+                label: 'Members',
+                to: '/settings/members',
+                onSelect: () => {
+                    open.value = false
+                }
+            }, {
+                label: 'Notifications',
+                to: '/settings/notifications',
+                onSelect: () => {
+                    open.value = false
+                }
+            }, {
+                label: 'Security',
+                to: '/settings/security',
+                onSelect: () => {
+                    open.value = false
+                }
+            }]
+        }
+    ],
+] satisfies NavigationMenuItem[][]
+
+const groups = computed(() => [{
+    id: 'links',
+    label: 'Aller à',
+    items: links.flat()
+}, ])
+
+
+</script>
+
 <template>
-  
+    <UDashboardGroup unit="rem">
+        <UDashboardSidebar id="default" v-model:open="open" collapsible resizable class="bg-elevated/25"
+            :ui="{ footer: 'lg:border-t lg:border-default' }">
+            <template #header="{ collapsed }">
+                <TeamsMenu :collapsed="collapsed" />
+            </template>
+
+            <template #default="{ collapsed }">
+                <UDashboardSearchButton :collapsed="collapsed" class="bg-transparent ring-default" />
+
+                <UNavigationMenu color="brand" :collapsed="collapsed" :items="links[0]" orientation="vertical" tooltip popover />
+
+                <UNavigationMenu color="brand" :collapsed="collapsed" :items="links[1]" orientation="vertical" tooltip
+                    class="mt-auto" />
+            </template>
+
+            <template #footer="{ collapsed }">
+                <UserMenu :collapsed="collapsed" />
+            </template>
+        </UDashboardSidebar>
+
+        <UDashboardSearch :groups="groups" />
+
+        <slot />
+
+    </UDashboardGroup>
 </template>
