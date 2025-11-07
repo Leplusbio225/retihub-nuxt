daa54ae6b0b61d8bb20c4ca2d687d7811725ed4a
Leplusbio225 <emmanuelkacou225@outlook.com>
2025-11-07 06:52:45 +0000

    feat: ajout d'une page d'erreur 404 avec gestion des métadonnées SEO

---
+++ app/error.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <UApp>
+        <UError :error="{
+            statusCode: 404, statusMessage: 'Page non trouvé',
+            message: 'La page que vous tenter de visiter n\'existe pas'}"/>
+    </UApp>
+
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+defineProps<{
+    error: NuxtError
+}>()
+
+useSeoMeta({
+    title: 'Page non trouvé',
+    description: "La page que vous tenter de visiter n'existe pas."
+})
+
+
+useHead({
+    htmlAttrs: {
+        lang: 'fr'
+    }
+})
+
+</script>
