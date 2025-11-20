02ddccc356c1ec2916c06478792c0454c4e49cb5
Leplusbio225 <emmanuelkacou225@outlook.com>
2025-11-20 19:31:47 +0000

    Refactor code structure for improved readability and maintainability

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,13 +1,7 @@
 <template>
     <nav style="text-shadow: 1px 1px 1px #000"
         class="py-2 px-4 border-[#bf6bc798] border text-[18px] shadow flex justify-between items-center text-white mx-2 rounded-full backdrop-blur-2xl bg-[#ffffff0d]">
-        <div class="text-2xl text-[#BF6BC7] font-bold">
-            <NuxtLink to="/" class="flex items-center">
-                RetiHub
-                <img width="25" src="~/assets/svg/logo.svg" alt="logo RetiHub" />
-            </NuxtLink>
-
-        </div>
+        <Logo/>
 
         <div class="flex gap-4">
             <NuxtLink to="/">Accueil</NuxtLink>
@@ -16,7 +10,7 @@
         </div>
 
         <div>
-            <NuxtLink to="#">Mon espace</NuxtLink>
+            <NuxtLink to="/dashboard">Mon espace</NuxtLink>
         </div>
     </nav>
 </template>
