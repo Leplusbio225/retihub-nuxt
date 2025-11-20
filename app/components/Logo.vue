f422dbb8e7284974707cc7c30fec7ba5bb6f9710
Leplusbio225 <emmanuelkacou225@outlook.com>
2025-11-20 19:32:18 +0000

    feat: ajout du composant Logo avec lien vers la page d'accueil

---
+++ app/components/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-2xl text-[#BF6BC7] font-bold">
+        <NuxtLink to="/" class="flex items-center">
+            RetiHub
+            <img width="25" src="~/assets/svg/logo.svg" alt="logo RetiHub" />
+        </NuxtLink>
+
+    </div>
+</template>
+
+<script setup>
+</script>
